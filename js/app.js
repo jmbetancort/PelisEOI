5695b4fb6eef42261f5d08553daae7dc510e77db
Jose Miguel <josemiguelbf@hotmail.es>
2017-05-23 13:55:09 +0100

    realizando maquetacion

---
+++ js/app.js
@@ -0,0 +1,20 @@
+(function() {
+    'use strict';
+
+    angular.module('JMAPP', ['ngRoute']).config(config);
+    config.$inject=['$routeProvider','$locationProvider'];
+    function config($routeProvider, $locationProvider){
+        $locationProvider.html5Mode(true);
+        $routeProvider
+            .when("/",{
+                controller: 'HomeController',
+                templateUrl: 'views/home.html',
+                controllerAs: 'Home'
+            })
+            .when("/individual.html",{
+                controller: 'IndividualController',
+                templateUrl: 'views/individual.html'
+            })
+            .otherwise( { redirectTo: "/"});
+    }
+})();
---
+++ js/controllers/home.controller.js
@@ -0,0 +1,18 @@
+(function() {
+'use strict';
+
+    angular
+        .module('JMAPP')
+        .controller('HomeController', HomeController);
+
+    HomeController.$inject = [];
+    function HomeContHomeller() {
+        var Home = this;
+
+        activate();
+
+        ////////////////
+
+        function activate() { }
+    }
+})();
