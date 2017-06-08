e7d39182e5e841f6fbef0105d0f084adbe0d3bf0
Jose Miguel <josemiguelbf@hotmail.es>
2017-06-08 21:49:01 +0100

    ....

--- js/controllers/main.controller.js
+++ js/controllers/main.controller.js
@@ -226,9 +226,7 @@
                             for (var i = Main.incremento; i < Main.incremento + 18; i++) {
                                 filmsMostrar.push(Main.films[i]);
                             }
-                            filmsMostrar.forEach(function(element,position){
-                                Main.filmsMostrar.push(element);
-                            })
+                            Main.filmsMostrar.push(filmsMostrar);
                             console.log(Main.filmsMostrar);
                             Main.incremento = Main.incremento + 18;
                         }
