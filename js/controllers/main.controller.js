aa85b8e4cec57597f6caa885b361219dea638a23
Jose Miguel <josemiguelbf@hotmail.es>
2017-06-08 21:34:14 +0100

    ....

--- js/controllers/main.controller.js
+++ js/controllers/main.controller.js
@@ -223,7 +223,7 @@
                             Main.filmsMostrar = Main.films;
                         } else {
                             for (var i = Main.incremento; i < Main.incremento + 18; i++) {
-                                Main.filmsMostrar.splice(i,0,films[i]);
+                                Main.filmsMostrar.splice(i,0,Main.films[i]);
                             }
                             console.log(Main.filmsMostrar);
                             Main.incremento = Main.incremento + 18;
